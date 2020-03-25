a12d0de21f140f11d160b65fa0013f811465390b
Rafael Toledo <rafaeldtoledo@gmail.com>
2020-03-25 00:09:50 -0300

    Updates to Kotlin 1.3.71

--- ios/Kotlin/ViewController.swift
+++ ios/Kotlin/ViewController.swift
@@ -42,7 +42,7 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.listing.count
+        self.listing.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
